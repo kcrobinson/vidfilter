8ca80301d15a7e997affdfc1f9da5beb5952dad3
kcrobinson@gmail.com <kcrobinson@gmail.com@69b3734c-8834-e434-d0bf-3890f39507b2>
2012-04-25 05:04:47 +0000

    Refactor Model into repository. No need for them to be decoupled.

---
+++ source/VidFilter.Repository/Model/Resolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VidFilter.Model
+{
+    public class Resolution : IMergeable
+    {
+        public string Id { get; set; }
+        public int PixelWidth { get; set; }
+        public int PixelHeight { get; set; }
+
+        public void Merge(object newObject, bool IncludeId = false)
+        {
+            Resolution newResolution = newObject as Resolution;
+            if (newResolution == null)
+            {
+                throw new NotSupportedException("Cannot update Resolution record with non-Resolution object");
+            }
+            if (IncludeId)
+            {
+                this.Id = newResolution.Id;
+            }
+            this.PixelHeight = newResolution.PixelHeight;
+            this.PixelWidth = newResolution.PixelWidth;
+        }
+    }
+}
