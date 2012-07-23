92a1c131f2e319cd16f34ce8c26bfe7e60b15c73
kcrobinson@gmail.com <kcrobinson@gmail.com@69b3734c-8834-e434-d0bf-3890f39507b2>
2012-07-23 23:04:03 +0000

    Adding missing UI files

---
+++ source/VidFilter/AdminPanel.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using VidFilter.Engine;
+using System.Configuration;
+using VidFilter.Repository.Model;
+
+namespace VidFilter
+{
+    /// <summary>
+    /// Interaction logic for AdminPanel.xaml
+    /// </summary>
+    public partial class AdminPanel : UserControl
+    {
+        MainWindowModel MainModel = MainWindow.MainModel;
+
+        public AdminPanel()
+        {
+            InitializeComponent();
+            this.DataContext = MainModel;
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            var denormalizedMovie = MainModel.Selected;
+            Movie movie = new Movie(denormalizedMovie.FullPath);
+            movie.FrameRate = denormalizedMovie.FrameRate;
+            movie.PlayLength = denormalizedMovie.PlayLength;
+            movie.ResolutionHeight = denormalizedMovie.ResolutionHeight;
+            movie.ResolutionWidth = denormalizedMovie.ResolutionWidth;
+
+            HelperMethods.InsertMovieToDatabase(movie);
+            MainModel.RefreshFromDatabase();
+        }
+
+        private void LoadMovieRowButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadMovie(LoadMovieRowTextBox.Text);
+            LoadMovieRowTextBox.Text = String.Empty;
+        }
+
+        private void LoadMovie(string filePath)
+        {
+            if (!File.Exists(filePath)) return;
+            
+            Engine.ProbeRequest request = new Engine.ProbeRequest();
+            request.FilePath = filePath;
+            Engine.ProbeResult result = App.Engine.ProbeVideoFile(request);
+            
+            if (!result.IsSuccess) return;
+
+            FileInfo file = new FileInfo(filePath);
+            MainModel.Selected = new DenormalizedMovie()
+            {
+                FileName = file.Name,
+                Directory = file.DirectoryName,
+                FrameRate = result.FrameRate,
+                ResolutionWidth = result.ResolutionWidth,
+                ResolutionHeight = result.ResolutionHeight,
+                PlayLength = result.PlayLength,
+                // colorspace
+            };
+        }
+
+        private void MovieSelectorListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void RemoveMovieButton_Click(object sender, RoutedEventArgs e)
+        {
+            var friendlyName = MovieSelectorListBox.SelectedItem as FriendlyName;
+            if (friendlyName == null) return;
+            App.Database.DeleteMovie(friendlyName.Id);
+            MainModel.RefreshFromDatabase();
+        }
+    }
+}
