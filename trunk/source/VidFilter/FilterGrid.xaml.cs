cc91187aa68ab5d6986f6582c2a84b4a46440ff0
kcrobinson@gmail.com <kcrobinson@gmail.com@69b3734c-8834-e434-d0bf-3890f39507b2>
2012-07-23 23:04:03 +0000

    Adding missing UI files

---
+++ trunk/source/VidFilter/FilterGrid.xaml.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VidFilter.Engine;
+using System.IO;
+using System.Configuration;
+using Model = VidFilter.Repository.Model;
+using VidFilter.Repository.Model;
+
+namespace VidFilter
+{
+    /// <summary>
+    /// Interaction logic for FilterGrid.xaml
+    /// </summary>
+    public partial class FilterGrid : UserControl
+    {
+        MainWindowModel MainModel = MainWindow.MainModel;
+
+        public FilterGrid()
+        {
+            InitializeComponent();
+            this.DataContext = MainModel;
+        }
+
+        #region Event Handlers
+
+        private void MovieSelectorListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FriendlyName movieItem = MovieSelectorListBox.SelectedItem as FriendlyName;
+            if (movieItem == null)
+            {
+                MainModel.Selected = null;
+                return;
+            }
+
+            try
+            {
+                MainModel.Selected = App.Database.QueryMovie(movieItem.Id, allowException: MainModel.IsDebug);
+            }
+            catch (Exception ex)
+            {
+                MainModel.AddDebugInfo("Failure getting movie from database", ex);
+                return;
+            }
+            if (MainModel.Selected == null)
+            {
+                return;
+            }
+
+            MovieDataGrid.Visibility = System.Windows.Visibility.Visible;
+            MovieFileNameValue.Content = MainModel.Selected.FileName;
+            MovieFullPathValue.Content = MainModel.Selected.Directory;
+            MovieFrameRateValue.Content = MainModel.Selected.FormattedFramerate;
+            MovieResolutionValue.Content = MainModel.Selected.FormattedResolution;
+            Colorspace colorspace = MainModel.Selected.Colorspace;
+            MovieColorspaceValue.Content = colorspace != null ? colorspace.Name : null;
+
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.UriSource = new Uri(MainModel.Selected.SampleImagePath, UriKind.Absolute);
+            src.EndInit();
+
+            string fullPath = MainModel.Selected.Directory;
+            if (!string.IsNullOrWhiteSpace(fullPath))
+            {
+                FileInfo fileInfo = new FileInfo(fullPath);
+                MovieFullPathChange.Text = fileInfo.DirectoryName;
+            }
+
+            int frameRate = MainModel.Selected.FrameRate;
+            if (frameRate > 0)
+            {
+                MovieFrameRateChangeSlider.Maximum = frameRate;
+                MovieFrameRateChangeSlider.Minimum = 1;
+                MovieFrameRateChangeSlider.SmallChange = 1;
+                MovieFrameRateChangeSlider.Value = MovieFrameRateChangeSlider.Maximum;
+            }
+
+            int width = MainModel.Selected.ResolutionWidth;
+            int height = MainModel.Selected.ResolutionHeight;
+            if (width > 0 && height > 0)
+            {
+                int gcd = 1;
+                for (int i = 2; i < Math.Min(width, height); i++)
+                {
+                    if (width % i == 0 && height % i == 0)
+                    {
+                        gcd = i;
+                    }
+                }
+                MovieResolutionGCD.Content = gcd;
+                MovieResolutionChangeSlider.Maximum = gcd;
+                MovieResolutionChangeSlider.Minimum = 1;
+                MovieResolutionChangeSlider.SmallChange = 1;
+                MovieResolutionChangeSlider.Value = MovieResolutionChangeSlider.Maximum;
+            }
+
+            foreach (var item in MovieColorspaceChange.Items)
+            {
+                string colorspaceItem = item as string;
+                if (String.Equals(colorspace.Name, colorspaceItem, StringComparison.OrdinalIgnoreCase))
+                {
+                    MovieColorspaceChange.SelectedItem = item;
+                    break;
+                }
+            }
+        }
+
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            FriendlyName movieItem = MovieSelectorListBox.SelectedItem as FriendlyName;
+            if (movieItem == null)
+            {
+                MainModel.AddDebugInfo("Not a valid movie selction");
+                return;
+            }
+
+            DenormalizedMovie movie = MainModel.Selected;
+            if (movie == null)
+            {
+                MainModel.AddDebugInfo("No movie selected");
+                return;
+            }
+
+            MainModel.AddDebugInfo("Processing movie at " + movie.FullPath);
+            EngineRequest engineRequest = new EngineRequest()
+            {
+                InputFileName = movie.FileName,
+                InputDirectory = movie.Directory,
+                // InputFrameRate = movie.FrameRate,
+                // InputHeight = movie.ResolutionHeight,
+                // InputWidth = movie.ResolutionWidth,
+                OutputFrameRate = IntTryParse(MovieFrameRateChangeText),
+                OutputHeight = IntTryParse(MovieResolutionHeightChangeText),
+                OutputWidth = IntTryParse(MovieResolutionWidthChangeText),
+                // OutputPath = string.IsNullOrWhiteSpace()
+                OutputCodec = "rawvideo",
+                OutputColorspace = MovieColorspaceChange.SelectedItem.ToString(),
+            };
+            EngineResult engineResult = App.Engine.ProcessRequest(engineRequest);
+            if (!engineResult.IsSuccess)
+            {
+                if (engineResult.Exception != null)
+                {
+                    MainModel.AddDebugInfo("Exception thrown", engineResult.Exception);
+                }
+                else
+                {
+                    MainModel.AddDebugInfo("Failure processing movie");
+                    MainModel.AddDebugInfo("Message: " + engineResult.Message);
+                    MainModel.AddDebugInfo("Std Out: " + engineResult.StdOutput);
+                    MainModel.AddDebugInfo("StdErr: " + engineResult.StdError);
+                }
+                return;
+            }
+            MainModel.AddDebugInfo("Process succeeded.");
+
+            MainModel.AddDebugInfo("Probing process result");
+            ProbeRequest probeRequest = new ProbeRequest()
+            {
+                FilePath = engineResult.OutFile.FullName
+            };
+            ProbeResult probeResult = App.Engine.ProbeVideoFile(probeRequest);
+            if (!probeResult.IsSuccess)
+            {
+                MainModel.AddDebugInfo("Failure: " + probeResult.ErrorMessage);
+                return;
+            }
+            Movie newMovie = new Movie(engineResult.OutFile);
+            newMovie.FrameRate = probeResult.FrameRate;
+            newMovie.ResolutionHeight = probeResult.ResolutionHeight;
+            newMovie.ResolutionWidth = probeResult.ResolutionWidth;
+            // newMovie.ColorSpaceId = probeResult.Colorspace;
+            newMovie.BitRate = probeResult.BitRate;
+            newMovie.PlayLength = probeResult.PlayLength;
+            newMovie.ParentMovieId = movie.MovieId;
+
+            ImageRequest imageRequest = new ImageRequest()
+            {
+                MovieLength = probeResult.PlayLength,
+                MoviePath = newMovie.FullName
+            };
+            ImageResult imageResult = App.Engine.CreateImage(imageRequest);
+            if (!imageResult.IsSuccess)
+            {
+                MainModel.AddDebugInfo(imageResult.ErrorMessage);
+                return;
+            }
+            Model.Image image = new Model.Image(imageResult.OutFile)
+            {
+                //ColorSpaceId = 
+                ResolutionHeight = probeResult.ResolutionHeight,
+                ResolutionWidth = probeResult.ResolutionWidth
+            };
+            MainModel.AddDebugInfo();
+            InsertImageToDatabase(image);
+            newMovie.SampleFrameId = image.Id;
+            InsertMovieToDatabase(newMovie);
+        }
+
+        private int IntTryParse(TextBox textBox)
+        {
+            int value = 0;
+            int.TryParse(textBox.Text, out value);
+            return value;
+        }
+
+        private void MovieFrameRateChangeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int value = (int)MovieFrameRateChangeSlider.Value;
+            MovieFrameRateChangeText.Text = value.ToString();
+        }
+
+        private void MovieFrameRateChangeText_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            int origValue = (int)MovieFrameRateChangeSlider.Value;
+            int newValue = 0;
+            int.TryParse(MovieFrameRateChangeText.Text, out newValue);
+
+            if (newValue < MovieFrameRateChangeSlider.Minimum || newValue > MovieFrameRateChangeSlider.Maximum)
+            {
+                MovieFrameRateChangeText.Text = origValue.ToString();
+            }
+            else
+            {
+                MovieFrameRateChangeSlider.Value = newValue;
+            }
+        }
+
+        private void MovieResolutionChangeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int value = (int)MovieResolutionChangeSlider.Value;
+            int gcd = (int)MovieResolutionGCD.Content;
+            MovieResolutionWidthChangeText.Text = (MainModel.Selected.ResolutionWidth * value / gcd).ToString();
+            MovieResolutionHeightChangeText.Text = (MainModel.Selected.ResolutionHeight * value / gcd).ToString();
+        }
+
+        private void MovieResolutionWidthChangeText_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            int newValue = 0;
+            int.TryParse(MovieResolutionWidthChangeText.Text, out newValue);
+            if (newValue != 0)
+            {
+                int gcd = (int)MovieResolutionGCD.Content;
+                double estimate = newValue * gcd / MainModel.Selected.ResolutionWidth;
+                MovieResolutionChangeSlider.Value = Math.Round(estimate);
+            }
+            else
+            {
+                MovieResolutionChangeSlider_ValueChanged(null, null);
+            }
+        }
+
+        private void MovieResolutionHeightChangeText_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            int newValue = 0;
+            int.TryParse(MovieResolutionHeightChangeText.Text, out newValue);
+            if (newValue != 0)
+            {
+                int gcd = (int)MovieResolutionGCD.Content;
+                double estimate = newValue * gcd / MainModel.Selected.ResolutionHeight;
+                MovieResolutionChangeSlider.Value = Math.Round(estimate);
+            }
+            else
+            {
+                MovieResolutionChangeSlider_ValueChanged(null, null);
+            }
+        }
+
+        #endregion
+
+        private void InsertMovieToDatabase(Movie movie)
+        {
+            MainModel.AddDebugInfo("Inserting movie in database");
+            OperationStatus status = App.Database.InsertMovie(movie);
+            if (!status.IsSuccess)
+            {
+                if (status.Exception != null)
+                {
+                    MainModel.AddDebugInfo("Exception thrown while inserting movie", status.Exception);
+                }
+                else
+                {
+                    MainModel.AddDebugInfo("Failure inserting movie");
+                    MainModel.AddDebugInfo("Message: " + status.Message);
+                }
+                return;
+            }
+            MainModel.AddDebugInfo("Success");
+            MainModel.Movies.Add(status.ResultingFriendlyName);
+        }
+
+        private void InsertImageToDatabase(Model.Image image)
+        {
+            MainModel.AddDebugInfo("Inserting image in database");
+            OperationStatus status = App.Database.InsertImage(image);
+            if (!status.IsSuccess)
+            {
+                if (status.Exception != null)
+                {
+                    MainModel.AddDebugInfo("Exception thrown while inserting image", status.Exception);
+                }
+                else
+                {
+                    MainModel.AddDebugInfo("Failure inserting image");
+                    MainModel.AddDebugInfo("Message: " + status.Message);
+                }
+                return;
+            }
+            MainModel.AddDebugInfo("Success");
+        }
+
+        private void ValueTextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (sender != null)
+            {
+                textBox.SelectAll();
+            }
+        }
+
+        private void ViewImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            showImageWindow = !showImageWindow;
+            if (showImageWindow)
+            {
+                ImageWindow.Show();
+                SetImageWindow();
+            }
+            else
+            {
+                DisposeImageWindow();
+            }
+        }
+
+        private ImageWindow _ImageWindow;
+        public ImageWindow ImageWindow
+        {
+            get
+            {
+                if (_ImageWindow == null && showImageWindow)
+                {
+                    _ImageWindow = new ImageWindow();
+                    _ImageWindow.Closed += OnImageWindowClose;
+                }
+                return _ImageWindow;
+            }
+        }
+
+        private bool showImageWindow = false;
+
+        private void SetImageWindow()
+        {
+            if (!ImageWindow.IsVisible)
+            {
+                return;
+            }
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(MainModel.Selected.SampleImagePath, UriKind.Absolute);
+            bitmap.EndInit();
+
+            ImageWindow.SampleVideoFrame = new System.Windows.Controls.Image()
+            {
+                Source = bitmap,
+                Width = MainModel.Selected.ResolutionWidth,
+                Height = MainModel.Selected.ResolutionHeight,
+                Focusable = true,
+                Visibility = Visibility.Visible
+            };
+            ImageWindow.UpdateLayout();
+            bool focus = ImageWindow.SampleVideoFrame.Focus();
+        }
+
+        private void DisposeImageWindow()
+        {
+            if (_ImageWindow != null)
+            {
+                ImageWindow.Close();
+                _ImageWindow = null;
+            }
+            this.showImageWindow = false;
+        }
+
+        private void OnImageWindowClose(object sender, EventArgs e)
+        {
+            this.DisposeImageWindow();
+        }
+    }
+}
---
+++ trunk/source/VidFilter/HelperMethods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VidFilter.Engine;
+using VidFilter.Repository.Model;
+
+namespace VidFilter
+{
+    class HelperMethods
+    {
+        public static string InsertMovieToDatabase(Movie movie)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ImageRequest imageRequest = new ImageRequest()
+            {
+                MoviePath = movie.FullName,
+                MovieLength = movie.PlayLength
+            };
+            ImageResult imageResult = App.Engine.CreateImage(imageRequest);
+            if (!imageResult.IsSuccess)
+            {
+                sb.Append("Error creating image: " + imageResult.ErrorMessage);
+            }
+            Image image = new Image(imageResult.OutFile)
+            {
+                ColorSpaceId = movie.ColorSpaceId,
+                ResolutionHeight = movie.ResolutionHeight,
+                ResolutionWidth = movie.ResolutionWidth
+            };
+            OperationStatus status = App.Database.InsertImage(image);
+            if (!status.IsSuccess)
+            {
+                sb.Append("Error inserting image to database: " + status.Message);
+                return sb.ToString();
+            }
+            movie.SampleFrameId = image.Id;
+
+            status = App.Database.InsertMovie(movie);
+            if (status.IsSuccess)
+            {
+                sb.Append("Success adding movie");
+            }
+            else
+            {
+                sb.Append(status.Message);
+                if (status.Exception != null)
+                {
+                    sb.Append("\r\n" + status.Exception.Message);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
