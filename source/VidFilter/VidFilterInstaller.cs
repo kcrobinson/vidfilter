1cc865ab926abd4fe4579553641982a48c96c81c
kcrobinson@gmail.com <kcrobinson@gmail.com@69b3734c-8834-e434-d0bf-3890f39507b2>
2012-07-28 23:34:30 +0000

    Creation of installer project

---
+++ source/VidFilter/VidFilterInstaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration;
+using System.Configuration.Install;
+
+namespace VidFilter
+{
+    [RunInstaller(true)]
+    public partial class VidFilterInstaller : Installer
+    {
+        public VidFilterInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            string targetDirectory = Context.Parameters["targetdir"];
+
+            string dataPath = Context.Parameters["DataPath"];
+            if (String.IsNullOrWhiteSpace(dataPath))
+            {
+                dataPath = @"data";
+            }
+
+            string exePath = string.Format("{0}VidFilter.exe", targetDirectory);
+            Configuration config = ConfigurationManager.OpenExeConfiguration(exePath);
+
+            config.AppSettings.Settings["EmbeddedConnectionPath"].Value = dataPath;
+            config.AppSettings.Settings["FfmpegDirectoryPath"].Value = String.Empty;
+            config.Save();
+        }
+    }
+}
